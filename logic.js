cb1ec52904c330d5debb17a2b5abe9a5ea648943
Dominik Vezmar <dominikvezmar1@gmail.com>
2024-05-06 14:13:29 +0200

    Add index.html and logic.js

--- logic.js
+++ logic.js
@@ -1,6 +1,36 @@
 const Answers = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
+let human = ""; // definiramo globalnu varijablu, mora biti definirana izvan funkcije
+
+//definiramo tipke
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
+
+//definiramo područje za prikaz rezultata
+const container = document.querySelector("#container");
+const score1 = document.createElement("div");
+score1.classList.add("content");
+const score2 = document.createElement("div");
+score2.classList.add("content");
+const winner = document.createElement("h2");
+winner.classList.add("content");
+
+rock.addEventListener('click', (event) => {
+    human = "rock"; // radimo samo update definirane varijable
+    playGame(); // pozivamo funkciju
+} );
+
+paper.addEventListener('click', (event) => {
+    human = "paper"; // radimo samo update definirane varijable
+    playGame(); // pozivamo funkciju
+});
+
+scissors.addEventListener('click', (event) => {
+    human = "scissors"; // radimo samo update definirane varijable
+    playGame(); // pozivamo funkciju
+});
 
 function getComputerChoice() {
     var random = Math.floor((Math.random()*Answers.length));
@@ -8,26 +38,10 @@ function getComputerChoice() {
     return randomAnswer;
 }
 
-function getHumanChoice() {
-    let human = prompt("What is your choice, rock, paper or scissors?")
-    switch(human){
-        case "rock":
-            console.log("You have selected rock!")
-            break;
-        case "paper":
-            console.log("You have selected paper!")
-            break;
-        case "scissors":
-            console.log("You have selected scissors!")
-            break;
-    }
-    return human;
-}
-
 function playRound(computerChoice, humanChoice){
     switch(computerChoice+ "|" +humanChoice){
         case "rock|scissors":
-            console.log("You loose, rock beats scissors!");
+            console.log("You lose, rock beats scissors!");
             computerScore++;
             break;
         case "rock|paper":
@@ -35,7 +49,7 @@ function playRound(computerChoice, humanChoice){
             humanScore++;
             break;
         case "scissors|paper":
-            console.log("You loose, scissors beats paper");
+            console.log("You lose, scissors beats paper");
             computerScore++;
             break;
         case "scissors|rock":
@@ -47,7 +61,7 @@ function playRound(computerChoice, humanChoice){
             humanScore++;
             break;
         case "paper|rock":
-            console.log("You loose, paper beats rock!");
+            console.log("You lose, paper beats rock!");
             computerScore++;
             break;
         case "rock|rock":
@@ -55,23 +69,38 @@ function playRound(computerChoice, humanChoice){
         case "scissors|scissors":
             console.log("It is a tie, play again!")
             break;
-
     }
-    return computerScore, humanScore;
+    return { computerScore, humanScore }; // vracamo rezultat
 }
+
+// trebamo logiku za rezultat postavit unutar ove funkcije jer su nam tu 
+//pozvane vec varijable humanScore i computerScore
 
 function playGame(){
     const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
-    playRound(computerSelection, humanSelection);
-    console.log(humanScore);
-    console.log(computerScore);
+    playRound(computerSelection, human);
+    score1.textContent = "Human score: " + humanScore;
+    score2.textContent= "Computer Score: " + computerScore;
+    if (humanScore === 5 && computerScore < 5){
+        winner.textContent = "You won!";
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore === 5 && humanScore < 5) {
+        winner.textContent = "Computer wins!";
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else {
+        winner.textContent = "Play!"
+    }
+
 }
 
-for (let i=0; i<5; i++){
-    playGame();
-}
 
 
 
 
+container.appendChild(score1);
+container.appendChild(score2);
+container.appendChild(winner);
